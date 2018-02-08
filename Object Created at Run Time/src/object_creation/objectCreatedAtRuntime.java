e95d47623b9ae795688f85f7bf928729a811103d
Jack Hosier <32852582+jackdaprogrammer@users.noreply.github.com>
2018-02-08 13:34:32 -0700

    Add files via upload

---
+++ Object Created at Run Time/src/object_creation/objectCreatedAtRuntime.java
@@ -0,0 +1,17 @@
+//Jack Hosier, Java Object Data 
+package object_creation;
+
+public class objectCreatedAtRuntime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		str = new String ("What up World?" );
+		
+		String secondVariable = "Hello, I am Microsoft Sam of Windows XP, the best text-to-speech voice from the best operating system in the world!";
+		System.out.println(str);
+		System.out.println(secondVariable);
+
+	}
+
+}
